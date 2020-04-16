8b663c620be76084682de0a4f83dd92835494f55
carey <schiafang@gmail.com>
2020-04-16 23:35:39 +0800

    1-2

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const valueR = document.querySelector('#input-r')
 const valueG = document.querySelector('#input-g')
 const valueB = document.querySelector('#input-b')
-const valueRGB = document.querySelector('.input-rgb')
 const valueHex = document.querySelector('#value-hex')
 const colorBoxR = document.querySelector('#box-r')
 const colorBoxG = document.querySelector('#box-g')
@@ -42,16 +41,44 @@ function switchBlueBox() {
   let b = Number(valueB.value)
   colorBoxB.style.backgroundColor = `#${RGBToHex(r, g, b)}`
 }
-//--------------- Handler ---------------//
-convertBtn.addEventListener('click', event => {
+//input數值限制並且不為空值
+function valueRange() {
   if ((Number(valueR.value) < 0 || Number(valueR.value) > 255 || (valueR.value) === '')
     || (Number(valueG.value) < 0 || Number(valueG.value) > 255 || (valueG.value) === '')
     || (Number(valueB.value) < 0 || Number(valueB.value) > 255 || (valueB.value) === '')) {
     alert('please enter number 0-255')
   }
+}
+//--------------- Handler ---------------//
+//監控convert按鈕
+convertBtn.addEventListener('click', event => {
+  valueRange()
 
   return switchToHex()
 })
+//監控input數值填完後任意三格enter鍵直接convert
+valueR.addEventListener('keypress', event => {
+  if (event.keyCode === 13) {
+    valueRange()
+
+    return switchToHex()
+  }
+})
+valueG.addEventListener('keypress', event => {
+  if (event.keyCode === 13) {
+    valueRange()
+
+    return switchToHex()
+  }
+})
+valueB.addEventListener('keypress', event => {
+  if (event.keyCode === 13) {
+    valueRange()
+
+    return switchToHex()
+  }
+})
+//監控輸入值不在0-255狀況，並即時預覽R.G.B.顏色
 valueR.addEventListener('input', event => {
   if (Number(valueR.value) < 0 || Number(valueR.value) > 255) { alert('please enter number 0-255') }
   switchRedBox()
@@ -64,4 +91,3 @@ valueB.addEventListener('input', event => {
   if (Number(valueB.value) < 0 || Number(valueB.value) > 255) { alert('please enter number 0-255') }
   switchBlueBox()
 })
-
